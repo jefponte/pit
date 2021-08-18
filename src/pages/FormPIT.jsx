9e5e02c8f116b7347f8161b54bae85b4c8dea6ce
Jefferson Uchôa Ponte <jefponte@gmail.com>
2021-08-17 21:14:06 -0300

    Autocomplete

--- src/pages/FormPIT.jsx
+++ src/pages/FormPIT.jsx
@@ -1,7 +1,20 @@
 import React, {Component} from 'react';
 import { Step, StepLabel, Stepper, TextField } from '@material-ui/core';
 import {Container, Typography, Button} from "@material-ui/core";
+import { Autocomplete } from "@material-ui/lab";
 import PanelPDF from '../components/PanelPDF';
+
+
+// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+const tiposAtividade = [
+    { title: 'ENSINO DE GRADUAÇÃO', id: 1 },
+    { title: 'ENSINO DE PÓS-GRADUAÇÃO STRICTO SENSU E LATO SENSU', id: 2 },
+    { title: 'ATIVIDADES COMPLEMENTARES DE ENSINO', id: 3 },
+    { title: 'PROGRAMAS E PROJETOS DE PESQUISA', id: 4 },
+    { title: 'PROGRAMAS E PROJETOS DE EXTENSÃO', id: 5 },
+    { title: "ATIVIDADES DE GESTÃO", id: 6 },
+    { title: 'OUTRAS ATIVIDADES RELEVANTES', id: 7 }
+  ];
 
 class FormPIT extends Component {
     
@@ -25,7 +38,9 @@ class FormPIT extends Component {
     handlerAvancar = (e) => {
         e.preventDefault();
         if(this.state.etapa >= 3){
-            alert("Esta é a última etapa.");
+            this.setState({
+                etapa: 0
+            });
             return;
         }
         const novaEtapa = this.state.etapa+1;
@@ -54,7 +69,9 @@ class FormPIT extends Component {
                     <>
                         <TextField value={this.state.nome} onChange={this.onChange('nome')} id="nome" label="Nome" variant="outlined" fullWidth margin="normal"/>
                         <TextField value={this.state.siape} onChange={this.onChange('siape')} id="siape" label="SIAPE" variant="outlined" fullWidth margin="normal"/>
-                        
+                        <Button onClick={this.handlerAvancar}  type="submit" variant="contained" color="primary">
+                            Avançar
+                        </Button>
                     </>
 
                 )
@@ -64,6 +81,9 @@ class FormPIT extends Component {
                     <>
                         <TextField value={this.state.periodo} onChange={this.onChange('periodo')} id="nome" label="Período Letivo" variant="outlined" fullWidth margin="normal"/>
                         <TextField value={this.state.regime} onChange={this.onChange('regime')} id="regime" label="Regime de Trabalho" variant="outlined" fullWidth margin="normal"/>
+                        <Button onClick={this.handlerAvancar}  type="submit" variant="contained" color="primary">
+                            Avançar
+                        </Button>
                     </>
 
                 );
@@ -71,7 +91,21 @@ class FormPIT extends Component {
             case 2:
                 return (
                     <>
-                        Teste<br/><br/>
+                        <Autocomplete
+                            id="tipo-atividade"
+                            options={tiposAtividade}
+                            getOptionLabel={(option) => option.title}
+                            fullWidth
+                            renderInput={(params) => <TextField {...params} label="Tipo de Atividade" variant="outlined" />}
+                            />
+
+                        <TextField  id="codigo" label="Código" variant="outlined" margin="normal"/>
+                        <TextField  id="disciplina" label="Disciplina" variant="outlined" margin="normal"/>
+                        <TextField  id="hs" label="Horas semanais" variant="outlined" fullWidth margin="normal"/>
+                        
+                        <Button onClick={this.handlerAvancar}  type="submit" variant="contained" color="primary">
+                            Avançar
+                        </Button>
                     </>
 
                 );
@@ -82,6 +116,7 @@ class FormPIT extends Component {
                             <Typography variant="h4" component="h2">
                             Verifique as informações e depois clique em Gerar PDF
                             </Typography><br/><br/>
+                            
                         </>
     
                     );
@@ -109,14 +144,6 @@ class FormPIT extends Component {
 
                 <form>
                     {this.formEtapa(this.state.etapa)}
-                    
-                    <Button onClick={this.handlerVoltar}  type="submit" variant="contained" color="primary">
-                        Voltar
-                    </Button>
-                    
-                    <Button onClick={this.handlerAvancar}  type="submit" variant="contained" color="primary">
-                        Avançar
-                    </Button>
                 </form>
                 {this.showPanelPDF(this.state.etapa)}
             </Container>
