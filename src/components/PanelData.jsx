30d35cb68889c4c0e981c148bed524b82718b98f
Jefferson Uchôa Ponte <jefponte@gmail.com>
2021-08-23 17:37:45 -0300

    Falta verificar porque da erro

--- src/components/PanelData.jsx
+++ src/components/PanelData.jsx
@@ -5,6 +5,12 @@ import {DataContext} from '../services/DataContext';
 import List from '@material-ui/core/List';
 import Element from '../components/Element';
 
+const tiposAtivComp = [
+    {descricao: "tutoria", sigla: "T"},
+    {descricao: "coordenação", sigla: "C"},
+    {descricao: "orientação", sigla: "O"}
+];
+
 const tiposAtividade = [
     { descricao: 'ENSINO DE GRADUAÇÃO', id: 0 },
     { descricao: 'ENSINO DE PÓS-GRADUAÇÃO STRICTO SENSU E LATO SENSU', id: 1},
@@ -25,14 +31,22 @@ class PanelData extends Component{
         this.state = {
             codigo: "", 
             disciplina: "", 
-            cargaHoraria: "",
-            tipo: null
+            horasSemanais: "",
+            tipoAtividade: null, 
+            tipo: null, 
+            programa: "", 
+            orientando: "", 
+            dataAprovacao: "", 
+            titulo: "", 
+            numeroPortaria: "",
+            data: "", 
+            cargoFuncao: "" 
 
         };
+        this.handleChange= this.handleChange.bind(this);
         this.handleAdd = this.handleAdd.bind(this);
-        this.onTagsChange = this.onTagsChange.bind(this);
-        this.handleChange = this.handleChange.bind(this);
         this.removeData = this.removeData.bind(this);
+        this.resetState = this.resetState.bind(this);
     }
     removeData(itemId){
         console.log(itemId);
@@ -46,23 +60,42 @@ class PanelData extends Component{
             {
                 codigo: "", 
                 disciplina: "", 
-                cargaHoraria: "",
-                tipo: null});
-        //
+                horasSemanais: "",
+                tipoAtividade: null, 
+                tipo: null, 
+                programa: "", 
+                orientando: "", 
+                dataAprovacao: "", 
+                titulo: "", 
+                numeroPortaria: "",
+                data: "", 
+                cargoFuncao: ""  
+            });
         
         
 
     }
     handleAdd(){
         const hora = this.state;
-        const newHora = Object.assign({}, hora, {id:(Date.now())});
+        const newData = Object.assign({}, hora, {id:(Date.now())});
 
-        this.context.data.push(newHora);
+        this.context.data.push(newData);
+        this.resetState();
+    }
+    resetState(){
         this.setState({
             codigo: "", 
             disciplina: "", 
-            cargaHoraria: "",
-            tipo: null
+            horasSemanais: "",
+            tipoAtividade: null, 
+            tipo: null, 
+            programa: "", 
+            orientando: "", 
+            dataAprovacao: "", 
+            titulo: "", 
+            numeroPortaria: "",
+            data: "", 
+            cargoFuncao: "" 
 
         });
     }
@@ -70,19 +103,21 @@ class PanelData extends Component{
         const target = event.target,
         value = target.type === 'checkbox' ? target.checked : target.value,
         name = target.name;
-
         this.setState({
             [name]: value
         });
+        
     }
-    onTagsChange = (event, values) => {
+    onTagsChangeTipoAtividade = (event, values) => {
+        this.setState({
+            tipoAtividade: values
+        });
+    }
+    onTagsChangeTipo = (event, values) => {
         this.setState({
             tipo: values
         });
     }
-
-
-
 
     render(){
         const {state} = this;
@@ -90,44 +125,106 @@ class PanelData extends Component{
             <>
                 <TextField value={state.codigo} name="codigo"  onChange={this.handleChange} id="codigo" label="Código" fullWidth variant="outlined" margin="normal"/>
                 <TextField value={state.disciplina} name="disciplina" onChange={this.handleChange} id="disciplina" label="Disciplina" fullWidth variant="outlined" margin="normal"/><br/>
-                <TextField value={state.cargaHoraria} name="cargaHoraria" onChange={this.handleChange} id="cargaHoraria" label="Carga Horária" fullWidth variant="outlined" margin="normal"/>
+                <TextField value={state.horasSemanais} name="horasSemanais" onChange={this.handleChange} id="horasSemanais" label="Horas Semanais" fullWidth variant="outlined" margin="normal"/>
                 <Button onClick={this.handleAdd}  type="submit" variant="contained" color="primary">
                     Adicionar
                 </Button>
             </>,
             <>
-            2
+                <TextField value={state.codigo} name="codigo"  onChange={this.handleChange} id="codigo" label="Código" fullWidth variant="outlined" margin="normal"/>
+                <TextField value={state.disciplina} name="disciplina" onChange={this.handleChange} id="disciplina" label="Disciplina" fullWidth variant="outlined" margin="normal"/><br/>
+                <TextField value={state.horasSemanais} name="horasSemanais" onChange={this.handleChange} id="horasSemanais" label="Horas Semanais" fullWidth variant="outlined" margin="normal"/>
+                <Button onClick={this.handleAdd}  type="submit" variant="contained" color="primary">
+                    Adicionar
+                </Button>
             </>,
             <>
-            3
+            
+           <Autocomplete 
+                margin="normal"
+                id="tipo"
+                options={tiposAtivComp}
+                getOptionLabel={option => option.descricao}
+                onChange={this.onTagsChangeTipo}
+                fullWidth
+                renderInput={(params) => <TextField {...params} label="Tipo de Atividade" variant="outlined" />}
+                /><br/>
+            <TextField value={state.programa} name="programa" onChange={this.handleChange} id="programa" label="Programa" fullWidth variant="outlined" margin="normal"/><br/>
+            <TextField value={state.programa} name="orientando" onChange={this.handleChange} id="orientando" label="Nome do Orientando" fullWidth variant="outlined" margin="normal"/>
+            <TextField value={state.horasSemanais} name="horasSemanais" onChange={this.handleChange} id="horasSemanais" label="Horas Semanais" fullWidth variant="outlined" margin="normal"/>
+            <Button onClick={this.handleAdd}  type="submit" variant="contained" color="primary">
+                Adicionar
+            </Button>
             </>,
             <>
-            4
+            <Autocomplete 
+                margin="normal"
+                id="tipo"
+                options={tiposAtivComp}
+                getOptionLabel={option => option.descricao}
+                onChange={this.onTagsChangeTipo}
+                fullWidth
+                renderInput={(params) => <TextField {...params} label="Tipo de Atividade" variant="outlined" />}
+                /><br/>
+            <TextField value={state.dataAprovacao} name="dataAprovacao"  onChange={this.handleChange} id="dataAprovacao" label="Data de Aprovação" fullWidth variant="outlined" margin="normal"/>
+            <TextField value={state.titulo} name="titulo" onChange={this.handleChange} id="titulo" label="Título" fullWidth variant="outlined" margin="normal"/><br/>
+            <TextField value={state.horasSemanais} name="horasSemanais" onChange={this.handleChange} id="horasSemanais" label="Horas Semanais" fullWidth variant="outlined" margin="normal"/>
+            <Button onClick={this.handleAdd}  type="submit" variant="contained" color="primary">
+                Adicionar
+            </Button>
             </>,
             <>
-            5
+            <Autocomplete 
+                margin="normal"
+                id="tipo"
+                options={tiposAtivComp}
+                getOptionLabel={option => option.descricao}
+                onChange={this.onTagsChangeTipo}
+                fullWidth
+                renderInput={(params) => <TextField {...params} label="Tipo de Atividade" variant="outlined" />}
+                /><br/>
+            <TextField value={state.dataAprovacao} name="dataAprovacao"  onChange={this.handleChange} id="dataAprovacao" label="Data de Aprovação" fullWidth variant="outlined" margin="normal"/>
+            <TextField value={state.titulo} name="titulo" onChange={this.handleChange} id="titulo" label="Título" fullWidth variant="outlined" margin="normal"/><br/>
+            <TextField value={state.horasSemanais} name="horasSemanais" onChange={this.handleChange} id="horasSemanais" label="Horas Semanais" fullWidth variant="outlined" margin="normal"/>
+            <Button onClick={this.handleAdd}  type="submit" variant="contained" color="primary">
+                Adicionar
+            </Button>
             </>, 
             <>
-            6
+            
+            <TextField value={state.numeroPortaria} name="numeroPortaria"  onChange={this.handleChange} id="numeroPortaria" label="Nº da Portaria" fullWidth variant="outlined" margin="normal"/>
+            <TextField value={state.data} name="data" onChange={this.handleChange} id="data" label="Data" fullWidth variant="outlined" margin="normal"/><br/>
+            <TextField value={state.cargoFuncao} name="cargoFuncao" onChange={this.handleChange} id="cargoFuncao" label="Cargo ou Função" fullWidth variant="outlined" margin="normal"/>
+            <TextField value={state.horasSemanais} name="horasSemanais" onChange={this.handleChange} id="horasSemanais" label="Horas Semanais" fullWidth variant="outlined" margin="normal"/>
+            <Button onClick={this.handleAdd}  type="submit" variant="contained" color="primary">
+                Adicionar
+            </Button>
             </>, 
             <>
-            7
+            
+            <TextField value={state.numeroPortaria} name="numeroPortaria"  onChange={this.handleChange} id="numeroPortaria" label="Nº da Portaria" fullWidth variant="outlined" margin="normal"/>
+            <TextField value={state.data} name="data" onChange={this.handleChange} id="data" label="Data" fullWidth variant="outlined" margin="normal"/><br/>
+            <TextField value={state.cargoFuncao} name="cargoFuncao" onChange={this.handleChange} id="cargoFuncao" label="Cargo ou Função" fullWidth variant="outlined" margin="normal"/>
+            <TextField value={state.horasSemanais} name="horasSemanais" onChange={this.handleChange} id="horasSemanais" label="Horas Semanais" fullWidth variant="outlined" margin="normal"/>
+            <Button onClick={this.handleAdd}  type="submit" variant="contained" color="primary">
+                Adicionar
+            </Button>
             </>];
         return (
         <>
 
 
-           <Autocomplete
-                id="tipo-atividade"
+           <Autocomplete 
+                margin="normal"
+                id="tipoAtividade"
                 options={tiposAtividade}
                 getOptionLabel={option => option.descricao}
-                onChange={this.onTagsChange}
-                value={this.state.tipo}
+                onChange={this.onTagsChangeTipoAtividade}
                 fullWidth
                 renderInput={(params) => <TextField {...params} label="Tipo de Atividade" variant="outlined" />}
-                />
+                /><br/>
                 
-            {(this.state.tipo)?formsData[this.state.tipo.id]:(<Typography variant="h4" component="h2">Selecione uma atividade</Typography>)}
+            {(this.state.tipoAtividade)?formsData[this.state.tipoAtividade.id]:(<Typography variant="h4" component="h2">Selecione uma atividade</Typography>)}
             
 
             <List>
---
+++ src/services/data.js
@@ -0,0 +1,35 @@
+{
+    docente: "Jose", siape: "123456", periodoLetivo: "2020.1", regimeTrabalho: "40horas"
+}
+
+
+relatorio = [
+    {
+        codigo:"CPT3345",
+        disciplina:"Tópicos especiais de Capueira I",
+        horasSemanais:20,
+        tipoAtividade:{descricao:"Ensino de Graduação", id: "1"},
+        tipo: {descricao: "coordenação", sigla:"C"},
+        programa: "PIBID",
+        orientando: "JEFFERSON UCHOA PONTE", 
+        dataAprovacao: "22/01/2019",
+        titulo: "Projeto Especial de Pesquisa I",
+        numeroPortaria: "123456",
+        data:"22/01/2020",
+        cargoFuncao: "Diretor"
+    }, 
+    {
+        codigo:"CPT3345",
+        disciplina:"Tópicos especiais de Capueira I",
+        horasSemanais:20,
+        tipoAtividade:{descricao:"Ensino de Graduação", id: "1"},
+        tipo: {descricao: "coordenação", sigla:"C"},
+        programa: "PIBID",
+        orientando: "JEFFERSON UCHOA PONTE", 
+        dataAprovacao: "22/01/2019",
+        titulo: "Projeto Especial de Pesquisa I",
+        numeroPortaria: "123456",
+        data:"22/01/2020",
+        cargoFuncao: "Diretor"
+    }
+]
